a2269e421af84d3ca23dd0d09cdad1248450a3ba
Jonathan Clarke <jonathan@beilabs.com>
2013-04-22 18:42:09 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weather::Application.config.secret_key_base = '6d689ab28eeb87d0b4a1685202273b234cdb76bf3d21a2f1c2290320727097ff40559ea2f5fc1cce8ded52079c68c2394653be96e10b9ea16bdcb3655aecb32a'
